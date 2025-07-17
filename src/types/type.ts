1124b44d3681b0e3531bb66708194af1249ed760
KaterynaBachkalo <kozlovets88@gmail.com>
2025-07-18 01:11:51 +0300

    feat: add types

--- src/types/type.ts
+++ src/types/type.ts
@@ -1,6 +1,14 @@
+import type { UseFormReturn } from "react-hook-form";
+
+import type { FormSchemaType } from "../components/cards/CreateCardModal";
+
 export type Payment = {
 	id: string;
 	default: boolean;
 	brand: "visa" | "mastercard" | "amex";
 	last4: string;
+};
+
+export type FormProps = {
+	form: UseFormReturn<FormSchemaType>;
 };
