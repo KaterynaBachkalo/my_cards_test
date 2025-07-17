88c9070ee909b9a191b6bbe5a359025a0f555101
KaterynaBachkalo <kozlovets88@gmail.com>
2025-07-17 16:05:05 +0300

    fix: app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { RouterProvider } from "@tanstack/react-router";
-import type { FunctionComponent } from "./common/types";
-import type { TanstackRouter } from "./main";
-import { TanStackRouterDevelopmentTools } from "./components/utils/development-tools/TanStackRouterDevelopmentTools";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { type JSX, lazy } from "react";
 
-const queryClient = new QueryClient();
+const Home = lazy(() => import("./pages/Home"));
+const CardList = lazy(() => import("./pages/CardList"));
 
-type AppProps = { router: TanstackRouter };
-
-const App = ({ router }: AppProps): FunctionComponent => {
+function App(): JSX.Element {
 	return (
-		<QueryClientProvider client={queryClient}>
-			<RouterProvider router={router} />
-			<TanStackRouterDevelopmentTools
-				initialIsOpen={false}
-				position="bottom-left"
-				router={router}
-			/>
-			<ReactQueryDevtools initialIsOpen={false} position="bottom" />
-		</QueryClientProvider>
+		<>
+			<Routes>
+				<Route element={<Home />} path="/" />
+				<Route element={<CardList />} path="my-cards" />
+				<Route element={<Navigate to="/" />} path="*" />
+			</Routes>
+		</>
 	);
-};
+}
 
 export default App;
