9515f70be9d32bb81897e1d2ace6a2e0201e9bc5
KaterynaBachkalo <kozlovets88@gmail.com>
2025-07-17 16:12:22 +0300

    feat: add mockData

---
+++ src/mockApi/mockData.ts
@@ -0,0 +1,24 @@
+import type { Payment } from "@/types/type";
+
+const initialData: Array<Payment> = [
+	{
+		id: "m5gr84i9",
+		default: true,
+		brand: "visa",
+		last4: "4242",
+	},
+	{
+		id: "3u1reuv4",
+		default: false,
+		brand: "mastercard",
+		last4: "6789",
+	},
+	{
+		id: "derv1ws0",
+		default: false,
+		brand: "amex",
+		last4: "1122",
+	},
+];
+
+export default initialData;
