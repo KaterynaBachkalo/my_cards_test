5891cc1c7859f000adb121113c3c896f1fcf3a9a
KaterynaBachkalo <kozlovets88@gmail.com>
2025-07-18 01:15:41 +0300

    feat: add component

---
+++ src/components/cards/FormCardNumber.tsx
@@ -0,0 +1,42 @@
+import type { JSX } from "react";
+
+import {
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from "../../../@/components/ui/form";
+
+import { Input } from "../../../@/components/ui/input";
+import type { FormProps } from "../../types/type";
+
+const FormCardNumber = ({ form }: FormProps): JSX.Element => {
+	return (
+		<FormField
+			control={form.control}
+			name="number"
+			render={({ field }) => (
+				<FormItem>
+					<FormLabel>Card Number</FormLabel>
+					<FormControl>
+						<Input
+							placeholder="1234 5678 9012 3456"
+							{...field}
+							maxLength={19}
+							value={field.value ?? ""}
+							onChange={(event) => {
+								const rawValue = event.target.value.replace(/\D/g, "");
+								const formatted = rawValue.match(/.{1,4}/g)?.join(" ") ?? "";
+								field.onChange(formatted);
+							}}
+						/>
+					</FormControl>
+					<FormMessage />
+				</FormItem>
+			)}
+		/>
+	);
+};
+
+export default FormCardNumber;
