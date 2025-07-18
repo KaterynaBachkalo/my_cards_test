26faaa2fabd751edfa4a705c32eed6401089e2d3
KaterynaBachkalo <kozlovets88@gmail.com>
2025-07-18 14:25:21 +0300

    fix: add types

--- src/common/types.ts
+++ src/common/types.ts
@@ -1,3 +1,7 @@
+import type { UseFormReturn } from "react-hook-form";
+
+import type { FormSchemaType } from "../components/cards/CreateCardModal";
+
 export type FunctionComponent = React.ReactElement | null;
 
 type HeroIconSVGProps = React.PropsWithoutRef<React.SVGProps<SVGSVGElement>> &
@@ -7,3 +11,19 @@ type IconProps = HeroIconSVGProps & {
 	titleId?: string;
 };
 export type Heroicon = React.FC<IconProps>;
+
+export type Payment = {
+	id: string;
+	default: boolean;
+	brand: "visa" | "mastercard" | "amex";
+	last4: string;
+};
+
+export type FormProps = {
+	form: UseFormReturn<FormSchemaType>;
+};
+
+export interface IContextTheme {
+	theme: "light" | "dark";
+	toggleTheme: () => void;
+}
