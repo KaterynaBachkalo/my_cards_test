3b0bae92dabdd1be0fefd46ae28c2b1d5eef176d
KaterynaBachkalo <kozlovets88@gmail.com>
2025-07-18 11:46:57 +0300

    feat: add theme

--- src/pages/CardList.tsx
+++ src/pages/CardList.tsx
@@ -1,10 +1,21 @@
 import { type JSX, Suspense, lazy } from "react";
+import SwitchThemeButtons from "../components/cards/SwitchThemeButtons";
+import { useTheme } from "../hooks/useTheme";
 
 const DataTable = lazy(() => import("../components/cards/DataTable"));
 
 const CardList = (): JSX.Element => {
+	const { theme } = useTheme();
+
+	if (theme === "dark") {
+		document.body.classList.add("dark");
+	} else {
+		document.body.classList.remove("dark");
+	}
+
 	return (
 		<main className="p-6">
+			<SwitchThemeButtons />
 			<h1 className="text-3xl font-bold mb-6">My Cards</h1>
 			<Suspense
 				fallback={
